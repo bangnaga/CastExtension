e716fea3735e2a936705f7a4e05c9af5f6c37c1b
yanquisalexander <alex.barrios2910@gmail.com>
2020-04-09 19:23:08 -0300

    .

--- src/uy/abstudios/cinelartv/mediaclient/CastExtension.java
+++ src/uy/abstudios/cinelartv/mediaclient/CastExtension.java
@@ -35,8 +35,7 @@ import com.google.appinventor.components.runtime.util.SdkLevel;
 import com.google.appinventor.components.runtime.util.ElementsUtil;
 import com.google.appinventor.components.runtime.util.YailList;
 import su.litvak.chromecast.api.v2.*;
-import javax.jmdns.*;
-import com.fasterxml.jackson.databind.*;
+import org.jmdns.*;
 import org.slf4j.*;
 
 
