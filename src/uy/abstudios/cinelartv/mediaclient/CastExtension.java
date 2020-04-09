d8a081d7bffaceae86b32d81c61cd0afe3d912fa
yanquisalexander <alex.barrios2910@gmail.com>
2020-04-09 20:50:08 -0300

    Add log4j

--- src/uy/abstudios/cinelartv/mediaclient/CastExtension.java
+++ src/uy/abstudios/cinelartv/mediaclient/CastExtension.java
@@ -37,6 +37,7 @@ import com.google.appinventor.components.runtime.util.YailList;
 import su.litvak.chromecast.api.v2.*;
 import javax.jmdns.*;
 import org.slf4j.*;
+import org.slf4j.impl.*;
 import com.google.protobuf.*;
 import com.fasterxml.jackson.databind.*;
 import com.fasterxml.jackson.core.*;
@@ -56,7 +57,7 @@ import com.fasterxml.jackson.annotation.*;
 
 
 
-@UsesLibraries(libraries = "api-v2-0.11.4-SNAPSHOT.jar,slf4j-api-1.7.30.jar,jmdns-3.5.5.jar,protobuf-java-2.6.0.jar,jackson-databind-2.10.3.jar,jackson-core-2.10.3.jar,jackson-annotations-2.10.3.jar")
+@UsesLibraries(libraries = "api-v2-0.11.4-SNAPSHOT.jar,slf4j-api-1.7.30.jar,log4j12-1.7.2.jar,jmdns-3.5.5.jar,protobuf-java-2.6.0.jar,jackson-databind-2.10.3.jar,jackson-core-2.10.3.jar,jackson-annotations-2.10.3.jar")
 @SimpleObject(external = true)
 public final class CastExtension extends AndroidNonvisibleComponent implements Component {
 
