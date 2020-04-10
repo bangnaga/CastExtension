01274cfedd94ed50d114c3ac1856bc46d0030834
yanquisalexander <alex.barrios2910@gmail.com>
2020-04-10 11:40:28 -0300

    Update CastExtension.java

--- src/uy/abstudios/cinelartv/mediaclient/CastExtension.java
+++ src/uy/abstudios/cinelartv/mediaclient/CastExtension.java
@@ -95,7 +95,7 @@ private static String APP_ID = "";
     public YailList getDeviceList() {
 
 
-        return makeList(java.util.List ChromeCasts.get())
+        return YailList.makeList(java.util.List ChromeCasts.get())
     
     }
 
