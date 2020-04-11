e8308d941225e590504bb166e1a032252a99b2ec
yanquisalexander <alex.barrios2910@gmail.com>
2020-04-10 22:35:46 -0300

    Update CastExtension.java

--- src/uy/abstudios/cinelartv/mediaclient/CastExtension.java
+++ src/uy/abstudios/cinelartv/mediaclient/CastExtension.java
@@ -35,6 +35,7 @@ import com.google.appinventor.components.common.YaVersion;
 import com.google.appinventor.components.runtime.util.SdkLevel;
 import com.google.appinventor.components.runtime.util.ElementsUtil;
 import com.google.appinventor.components.runtime.util.YailList;
+import com.google.appinventor.components.runtime.util;
 import su.litvak.chromecast.api.v2.*;
 import org.slf4j.*;
 import com.fasterxml.jackson.databind.*;
