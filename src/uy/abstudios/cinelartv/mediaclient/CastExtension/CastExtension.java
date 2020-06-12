2ed216cf153d56413c2ff2a88c28cd1509900777
Nathan <43486313+StormiFire@users.noreply.github.com>
2020-06-12 07:17:20 -0400

    Make code pretty and consistent 🤗

--- src/uy/abstudios/cinelartv/mediaclient/CastExtension/CastExtension.java
+++ src/uy/abstudios/cinelartv/mediaclient/CastExtension/CastExtension.java
@@ -1,13 +1,11 @@
 /**
-Authors: Alexander Barrios
-    
-Version alpha
-
-Extensión creada para la aplicación CinelarTV
-**/
+ * @author Alexander Barrios
+ * @version Alpha
+ * -------------
+ * Extensión creada para la aplicación CinelarTV 
+ */
 
 package uy.abstudios.cinelartv.mediaclient.CastExtension;
-
 
 import android.os.AsyncTask;
 import com.google.appinventor.components.annotations.*;
@@ -38,250 +36,220 @@ import javax.jmdns.ServiceListener;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
-
-
-
-
-
-@DesignerComponent(version = 1,
- category = ComponentCategory.EXTENSION,
- description = "Chromecast Extension for CinelarTV App",
- nonVisible = true,
- iconName = "https://cdn.iconscout.com/icon/free/png-256/cast-226401.png",
- helpUrl = "https://developers.cinelartv.tk/opensource/cast.html")
-@UsesLibraries(libraries = "api-v2-0.11.2-SNAPSHOT.jar,jackson-core-2.9.10.jar,jmdns-3.5.5.jar,slf4j-api-1.7.30.jar,jackson-annotations-2.9.10.jar,jackson-databind-2.9.10.4.jar,protobuf-java-2.6.0.jar,android-support-v4.jar,support-compat-28.0.0.jar")
+@DesignerComponent(
+	version = 1,
+	category = ComponentCategory.EXTENSION,
+	description = "Chromecast Extension for CinelarTV App",
+	nonVisible = true,
+	iconName = "https://cdn.iconscout.com/icon/free/png-256/cast-226401.png",
+	helpUrl = "https://developers.cinelartv.tk/opensource/cast.html"
+)
+@UsesLibraries(libraries = "api-v2-0.11.2-SNAPSHOT.jar, jackson-core-2.9.10.jar, jmdns-3.5.5.jar, slf4j-api-1.7.30.jar, jackson-annotations-2.9.10.jar ,jackson-databind-2.9.10.4.jar, protobuf-java-2.6.0.jar, android-support-v4.jar, support-compat-28.0.0.jar")
 @SimpleObject(external = true)
 @UsesPermissions(permissionNames = "android.permission.INTERNET")
-
 public final class CastExtension extends AndroidNonvisibleComponent implements Component {
+	private static String APP_ID = "";
+	private YailList extras;
+	private List<ChromeCast> castsList;
+	private List<ChromeCast> nativeList;
+	private ChromeCast chromecast;
+	private Context context;
+	private JmDNS mDNS;
+	private boolean hasListener;
 
+	public CastExtension(ComponentContainer container) {
+		super(container.$form());
+		context = container.$context();
+		castsList = new ArrayList<>();  
+	}
 
-private static String APP_ID = "";
-private YailList extras;
-private List<ChromeCast> castsList;
-private List<ChromeCast> nativeList;
-private ChromeCast chromecast;
-private Context context;
-private JmDNS mDNS;
-private boolean hasListener;
+	/**
+	 * --------------
+	 *   PROPERTIES
+	 * --------------
+	 */
+	@DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING, defaultValue = "")
+	@SimpleProperty
+	public void AppID(String applicationID) {
+		if(applicationID.equals("DEFAULT_MEDIA_RECEIVER_APPLICATION_ID")) {
+				APP_ID = "CC1AD845";
+		} else {
+				APP_ID = applicationID;
+		}
+	}
 
- public CastExtension(ComponentContainer container) {
-  super(container.$form());
-  context = container.$context();
-  castsList = new ArrayList<>();  
- }
+	/**
+	 * -------------
+	 *   FUNCTIONS
+	 * -------------
+	 */
+	@SimpleFunction(description = "Set Volume")
+	public void SetVolume(int volume) throws IOException {
+		chromecast.setVolume(volume);
+	}
 
+	@SimpleFunction(description = "")
+	public void Pause() throws IOException {
+		chromecast.pause();
+	}
 
- // ------------------------
-    //        PROPERTIES
-    // ------------------------
+	@SimpleFunction(description = "")
+	public void Play() throws IOException {
+		chromecast.play();
+	}
+	
+	@SimpleFunction(description = "")
+	public void Seek(int seek) throws IOException {
+		chromecast.seek(seek);
+	}
+	
+	@SimpleFunction(description = "")
+	public void SetMuted(boolean mute) throws IOException {
+		chromecast.setMuted(mute);
+	}
 
+	@SimpleFunction(description = "Start devices scan")
+	public void startDiscovery() {
+		new StartListening().execute();
+		appendListener();
+	}
 
+	@SimpleFunction(description = "Stop devices scan")
+	public void stopDiscovery() {
+		try {
+			ChromeCasts.stopDiscovery();
+			ChromeCasts.unregisterListener(listener);
+			hasListener = false;
+		} catch (Exception e) {
+			OnError("StoppingDiscoveringDevices", e.toString());
+			e.printStackTrace();
+		}
+	}
 
+	@SimpleFunction(description = "Restart devices scan")
+	public void RestartDiscovery() {
+		try {
+			castsList.clear();
+			stopDiscovery();
+			startDiscovery();
+		} catch (Exception e) {
+			OnError("RestartingDiscoveringDevices", e.toString());
+			e.printStackTrace();
+		}
+	}
+	
+	@SimpleFunction(description="Returns device status")
+	public String DeviceStatus() {
+		String status;
+		try {
+			status = chromecast.getStatus().toString();
+		} catch (IOException e) {
+			status = e.getMessage() + " , " + e.toString();
+			OnError("ObtainingDeviceStatus", status);
+			e.printStackTrace();
+		}
 
+		return status;
+	}
 
- @DesignerProperty(editorType = PropertyTypeConstants.PROPERTY_TYPE_STRING, defaultValue = "")
-    @SimpleProperty
-    public void AppID(String applicationID) {
-        if(applicationID.equals("DEFAULT_MEDIA_RECEIVER_APPLICATION_ID"))
-           APP_ID = "CC1AD845";
-        else
-           APP_ID = applicationID;
-    }
+	@SimpleFunction(description = "Show list of devices detected")
+	public List<ChromeCast> DevicesList() {
+		return castsList;
+	}
 
+	@SimpleFunction(description = "Launch Application in Connected Device") 
+	public void LaunchApp() {
+		String status;
+		try {
+			chromecast.launchApp(APP_ID);
+		} catch (IOException e) {
+			status = e.getMessage() + " , " + e.toString();
+			e.printStackTrace();
+		}
+	}
+	
+	@SimpleFunction
+	public int DiscoveredDevicesCount() {
+		return castsList.size();
+	}
+	
+	@SimpleFunction(description = "")
+	public void ConnectToDevice(int index) throws IOException, GeneralSecurityException {
+		chromecast = castsList.get(index);
+		chromecast.connect();
+	}
+	
+	@SimpleFunction(description = "")
+	public void ConnectToDeviceWithIP(String ipAddress) {
+		chromecast = new ChromeCast(ipAddress);
+		try {
+			chromecast.connect();
+		} catch(Exception e ){
+			e.printStackTrace();
+		}
+	}
+	
+	@SimpleFunction
+	public void SetMedia(String title, String thumbnail, String video) throws IOException {
+		chromecast.load(title, thumbnail, video, null);
+	}
+	
+	@SimpleEvent
+	public void OnError(String process, String message) {
+		ChromeCasts.unregisterListener(listener);
+		hasListener = false;
+		EventDispatcher.dispatchEvent(this, "OnError", process, message);
+	}
 
+	@SimpleEvent(description = "")
+	public void OnChromeCastDiscovered(String deviceName,String deviceAddress, int devicePort) {
+		EventDispatcher.dispatchEvent(this, "OnChromeCastDiscovered", deviceName, deviceAddress, devicePort);
+	}
 
+	@SimpleEvent(description = "")
+	public void OnNativeCastDiscovered(String deviceName,String deviceAddress, int devicePort) {
+		EventDispatcher.dispatchEvent(this, "OnNativeCastDiscovered", deviceName, deviceAddress, devicePort);
+	}
+	
+	@SimpleEvent(description="Cast removed from ")
+	public void OnChromeCastRemoved(String deviceName,String deviceAddress, int devicePort) {
+		EventDispatcher.dispatchEvent(this, "OnChromeCastRemoved", deviceName, deviceAddress, devicePort);
+	}
+	
+	public void appendListener() {
+		if (!hasListener) {
+			nativeList = ChromeCasts.get();
+			ChromeCasts.registerListener(listener);
+			hasListener = true;
+		}
+	}
 
-// ------------------------
-    //        Functions
-    // ------------------------
+	private ChromeCastsListener listener = new ChromeCastsListener() {
+		@Override public void newChromeCastDiscovered(ChromeCast chromeCast) {
+			castsList.add(chromeCast);
+			OnChromeCastDiscovered(chromeCast.getName(), chromeCast.getAddress(), chromeCast.getPort());
+		}
+		
+		@Override public void chromeCastRemoved(ChromeCast chromeCast) {
+			if (castsList.contains(chromeCast)) {
+					castsList.remove(chromeCast);
+			}
+			
+			OnChromeCastRemoved(chromeCast.getName(), chromeCast.getAddress(), chromeCast.getPort());
+		}
+	};    
 
+	class StartListening extends AsyncTask <Void, Void, Void> {
+		@Override
+		protected Void doInBackground(Void... args) {
+			try {
+					ChromeCasts.startDiscovery(); 
+			} catch (Exception e) {
+					OnError("StartingDiscoveryTask", e.toString());
+					e.printStackTrace();
+			}
 
-
-     @SimpleFunction(description = "Set Volume")
-    public void SetVolume(int volume) throws IOException {
-         chromecast.setVolume(volume);
-        }
-
-     @SimpleFunction(description="")
-     public void Pause() throws IOException {
-     chromecast.pause();
-     }
-
-     @SimpleFunction(description="")
-     public void Play() throws IOException {
-     chromecast.play();
-     }
-     
-     @SimpleFunction(description="")
-     public void Seek(int seek) throws IOException {
-     chromecast.seek(seek);
-     }
-     
-     @SimpleFunction(description="")
-     public void SetMuted(boolean mute) throws IOException {
-     chromecast.setMuted(mute);
-     }
-
-
-
-    @SimpleFunction(description = "Start devices scan")
-    public void startDiscovery() {
-    new StartListening().execute();
-    appendListener();
+			return null;
+		}
+	}   
 }
-
-
-
-
-
-
-    @SimpleFunction(description = "Stop devices scan")
-    public void stopDiscovery() {
-    try {
-    ChromeCasts.stopDiscovery();
-    ChromeCasts.unregisterListener(listener);
-        hasListener = false;
-    } catch (Exception e) {
-        OnError("StoppingDiscoveringDevices", e.toString());
-    e.printStackTrace();
-    }
-    }
- 
-    @SimpleFunction(description = "Restart devices scan")
-    public void RestartDiscovery() {
-    try {
-        castsList.clear();
-        stopDiscovery();
-        startDiscovery();
-    } catch (Exception e) {
-        OnError("RestartingDiscoveringDevices", e.toString());
-        e.printStackTrace();
-    }
-    }
-    
-    @SimpleFunction(description="Returns device status")
-    public String DeviceStatus(){
-        String status;
-        try
-        {
-            status = chromecast.getStatus().toString();
-        } catch (IOException e) {
-            status = e.getMessage() + " , " + e.toString();
-            OnError("ObtainingDeviceStatus", status);
-            e.printStackTrace();
-        }
-        return status;
-    }
-
-    @SimpleFunction(description = "Show list of devices detected")
-    public List<ChromeCast> DevicesList() {
-        return castsList;
-    }
-
-
-
-
-
-    @SimpleFunction(description = "Launch Application in Connected Device") 
-    public void LaunchApp() {
-    	String status;
-    	try {
-    		chromecast.launchApp(APP_ID);
-    	} 
-
-    	catch (IOException e) {
-            status = e.getMessage() + " , " + e.toString();
-            e.printStackTrace();
-        }
-    }
-
-
-
-
-
-
-
-    
-    @SimpleFunction
-    public int DiscoveredDevicesCount(){
-        return castsList.size();
-    }
-    
-    @SimpleFunction(description="")
-    public void ConnectToDevice(int index) throws IOException, GeneralSecurityException {
-    chromecast = castsList.get(index);
-    chromecast.connect();
-    }
-    
-    @SimpleFunction(description="")
-    public void ConnectToDeviceWithIP(String ipAddress){
-    chromecast = new ChromeCast(ipAddress);
-        try{
-            chromecast.connect();
-        } catch(Exception e ){
-            e.printStackTrace();
-        }
-    }
-    
-    @SimpleFunction
-    public void SetMedia(String title, String thumbnail, String video) throws IOException {
-    chromecast.load(title, thumbnail, video, null);
-    }
-    
-   @SimpleEvent
-    public void OnError(String process, String message){
-        ChromeCasts.unregisterListener(listener);
-        hasListener = false;
-        EventDispatcher.dispatchEvent(this, "OnError", process, message);
-    }
-   @SimpleEvent(description="")
-    public void OnChromeCastDiscovered(String deviceName,String deviceAddress, int devicePort){
-        EventDispatcher.dispatchEvent(this, "OnChromeCastDiscovered", deviceName, deviceAddress, devicePort);
-    }
-   @SimpleEvent(description="")
-    public void OnNativeCastDiscovered(String deviceName,String deviceAddress, int devicePort){
-        EventDispatcher.dispatchEvent(this, "OnNativeCastDiscovered", deviceName, deviceAddress, devicePort);
-    }
-    @SimpleEvent(description="Cast removed from ")
-    public void OnChromeCastRemoved(String deviceName,String deviceAddress, int devicePort){
-        EventDispatcher.dispatchEvent(this, "OnChromeCastRemoved", deviceName, deviceAddress, devicePort);
-    }
-    public void appendListener(){
-        if (!hasListener){
-            nativeList = ChromeCasts.get();
-            ChromeCasts.registerListener(listener);
-            hasListener = true;
-        }
-    }
-
-private ChromeCastsListener listener = new ChromeCastsListener(){
-            @Override public void newChromeCastDiscovered(ChromeCast chromeCast){
-                castsList.add(chromeCast);
-                OnChromeCastDiscovered(chromeCast.getName(), chromeCast.getAddress(), chromeCast.getPort());
-            }
-            
-            @Override public void chromeCastRemoved(ChromeCast chromeCast){
-                if (castsList.contains(chromeCast))
-                    castsList.remove(chromeCast);
-                OnChromeCastRemoved(chromeCast.getName(), chromeCast.getAddress(), chromeCast.getPort());
-            }
-        };    
-class StartListening extends AsyncTask <Void, Void, Void> {
-
-    	@Override protected Void doInBackground(Void... args){
-
-try {
-        ChromeCasts.startDiscovery(); 
-    } catch (Exception e) {
-        OnError("StartingDiscoveryTask", e.toString());
-        e.printStackTrace();
-
-    }
-    return null;
-    	}
-    }    
-    
-}
-  
-
